2b063a91bddd48e8b65c5746859a8246f1efc465
Alexey Zimarev <alex@zimarev.com>
2021-11-29 22:37:00 +0100

    Integration

---
+++ day2/03-integration/Bookings/HttpApi/Bookings/CommandApi.cs
@@ -0,0 +1,31 @@
+using Bookings.Domain.Bookings;
+using Eventuous;
+using Eventuous.AspNetCore.Web;
+using Microsoft.AspNetCore.Mvc;
+using static Bookings.Application.BookingCommands;
+
+namespace Bookings.HttpApi.Bookings;
+
+[Route("/booking")]
+public class CommandApi : CommandHttpApiBase<Booking> {
+
+    public CommandApi(IApplicationService<Booking> service) : base(service) { }
+
+    [HttpPost]
+    [Route("book")]
+    public Task<ActionResult<Result>> BookRoom([FromBody] BookRoom cmd, CancellationToken cancellationToken)
+        => Handle(cmd, cancellationToken);
+
+    [HttpPost]
+    [Route("discount")]
+    public Task<ActionResult<Result>> ApplyDiscount(
+        [FromBody] ApplyDiscount cmd,
+        CancellationToken        cancellationToken
+    )
+        => Handle(cmd, cancellationToken);
+
+    [HttpPost]
+    [Route("recordPayment")]
+    public Task<ActionResult<Result>> RecordPayment([FromBody] RecordPayment cmd, CancellationToken cancellationToken)
+        => Handle(cmd, cancellationToken);
+}
